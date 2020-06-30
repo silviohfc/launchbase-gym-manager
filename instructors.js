bc10035dd810ea63515db497724cf7f2fd7cb03e
Silvio Henrique <silviohfc@gmail.com>
2020-06-30 18:21:04 -0300

    Manipulating and showing instructors data

--- instructors.js
+++ instructors.js
@@ -1,5 +1,29 @@
+const Intl = require('intl')
 const fs = require('fs')
 const data = require('./data.json')
+const { age } = require('./utils')
+
+
+// Show
+exports.show = (req, res) => {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(instructor => {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+    }
+
+    return res.render("instructors/show", { instructor })
+}
 
 // Create
 exports.post = (req, res) => {
@@ -11,10 +35,21 @@ exports.post = (req, res) => {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, birth, name, services, gender } = req.body
 
-    data.instructors.push(req.body)
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send("Write file error!")
