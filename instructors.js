116c75f762973f9f1cab688a7ccfef20cca3fab1
Silvio Henrique <silviohfc@gmail.com>
2020-07-01 17:14:11 -0300

    Added instructor edit and delete features, created instructors list

--- instructors.js
+++ instructors.js
@@ -3,6 +3,10 @@ const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+exports.index = (req, res) => {
+    return res.render("instructors/index", { instructors: data.instructors })
+}
+
 
 // Show
 exports.show = (req, res) => {
@@ -77,6 +81,48 @@ exports.edit = (req, res) => {
 }
 
 // Update
+exports.put = (req, res) => {
+    const { id } = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
 
 // Delete
+exports.delete = (req, res) => {
+    const { id } = req.body
 
+    const filteredInstructors = data.instructors.filter(instructor => {
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), err => {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+    })
+}
--- routes.js
+++ routes.js
@@ -6,9 +6,7 @@ routes.get("/", (req, res) => {
     return res.redirect("/instructors")
 })
 
-routes.get("/instructors", (req, res) => {
-    return res.render("instructors/index")
-})
+routes.get("/instructors", instructors.index)
 
 routes.get("/instructors/create", (req, res) => {
     return res.render("instructors/create")
@@ -19,6 +17,10 @@ routes.get("/instructors/:id", instructors.show)
 routes.get("/instructors/:id/edit", instructors.edit)
 
 routes.post("/instructors", instructors.post)
+
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
 
 routes.get("/members", (req, res) => {
     return res.send("members")
