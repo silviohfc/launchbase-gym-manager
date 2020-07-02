fcbf778115407e60a6ba417525a22f93fdd1ccac
Silvio Henrique <silviohfc@gmail.com>
2020-07-02 15:31:42 -0300

    Created members area and controllers

--- controllers/instructors.js
+++ controllers/instructors.js
@@ -1,35 +1,19 @@
 const Intl = require('intl')
 const fs = require('fs')
-const data = require('./data.json')
-const { age, date } = require('./utils')
+const data = require('../data.json')
+const { age, date } = require('../utils')
 
 exports.index = (req, res) => {
     return res.render("instructors/index", { instructors: data.instructors })
 }
 
 
-// Show
-exports.show = (req, res) => {
-    const { id } = req.params
-
-    const foundInstructor = data.instructors.find(instructor => {
-        return instructor.id == id
-    })
-
-    if (!foundInstructor) return res.send("Instructor not found!")
-
-
-    const instructor = {
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-        services: foundInstructor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
-    }
-
-    return res.render("instructors/show", { instructor })
+// Create
+exports.create = (req, res) => {
+    return res.render("instructors/create")
 }
 
-// Create
+// Post
 exports.post = (req, res) => {
     const keys = Object.keys(req.body)
 
@@ -62,6 +46,27 @@ exports.post = (req, res) => {
     })
 }
 
+// Show
+exports.show = (req, res) => {
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(instructor => {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!")
+
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+    }
+
+    return res.render("instructors/show", { instructor })
+}
+
 // Edit
 exports.edit = (req, res) => {
     const { id } = req.params
@@ -74,7 +79,7 @@ exports.edit = (req, res) => {
 
     const instructor = {
         ...foundInstructor,
-        birth: date(foundInstructor.birth)
+        birth: date(foundInstructor.birth).iso
     }
 
     return res.render("instructors/edit", { instructor })
